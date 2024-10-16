ac382d2699e19b278be9deef181458efe447896e
shahjahah0011 <jahanshah2003@gmail.com>
2024-10-15 23:50:38 -0700

    implemet the complete layout : homepage UI + sidebars, routing other pages !

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,40 @@
+import RightSidebar from '@/components/RightSidebar'
+import HeaderBox from '@/components/ui/HeaderBox'
+import TotalBalanceBox from '@/components/ui/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = { firstName :'Jahan', lastName:'Shah', email:'contact@jahanshah.com'} 
+
   return (
-    <div>Home</div>
+    <section className="home">
+    <div className="home-content">
+      <header className="home-header">
+        <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and trasactions effectively."
+        />
+
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1023.45}
+        />
+      </header>
+
+      RECENT TRANSACTIONS
+      </div>
+
+        <RightSidebar 
+          user = {loggedIn}
+          transactions={[]}
+          banks={[{currentBalance:432.50},{currentBalance:450.60}]}
+        />
+
+      </section>
   )
 }
 
