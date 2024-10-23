94c6071b27f257787034f60cd98e68d0251ae91e
shahjahah0011 <jahanshah2003@gmail.com>
2024-10-22 19:19:56 -0700

    implement user personalization and data pulling for home page, unable to pull transactions

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -12,8 +12,9 @@ import {
 import { plaidClient } from "../plaid";
 import { parseStringify } from "../utils";
 
-// import { getTransactionsByBankId } from "./transaction.actions";
+
 import { getBanks, getBank } from "./user.actions";
+import { getTransactionsByBankId } from "./transaction.action";
 
 // Get multiple bank accounts
 export const getAccounts = async ({ userId }: getAccountsProps) => {
